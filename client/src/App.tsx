d1b60ff9d97d11b2c5fcd8c414a3b0028e5bea61
danielace1 <danielsudharsan1805@gmail.com>
2025-03-04 00:33:01 +0530

    server init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,22 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
 const App = () => {
-  return <div className="text-red-500 text-xl">App</div>;
+  return (
+    <div className="text-red-500 text-xl">
+      App
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
+
 export default App;
