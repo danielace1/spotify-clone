93900b6738d6c0a103c7fa46402f58916130fd1d
danielace1 <danielsudharsan1805@gmail.com>
2025-10-22 20:30:49 +0530

    feat: final commit

--- client/src/stores/useChatStore.ts
+++ client/src/stores/useChatStore.ts
@@ -22,7 +22,8 @@ interface ChatStore {
   setSelectedUser: (user: User | null) => void;
 }
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const baseURL =
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
 
 const socket = io(baseURL, {
   autoConnect: false,
--- server/src/index.js
+++ server/src/index.js
@@ -3,9 +3,10 @@ import "dotenv/config";
 import path from "path";
 import { createServer } from "http";
 import cors from "cors";
-// import { withAuth } from "@clerk/clerk-sdk-node";
 import { clerkMiddleware } from "@clerk/express";
+import fs from "fs";
 import fileUpload from "express-fileupload";
+import cron from "node-cron";
 
 import { initializeSocket } from "./lib/socket.js";
 import { connectDB } from "./lib/db.js";
@@ -42,12 +43,35 @@ app.use(
   })
 );
 
+// Schedule a cron job to clean up temp directory every hour
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+  if (fs.existsSync(tempDir)) {
+    fs.readdir(tempDir, (err, files) => {
+      if (err) {
+        console.log("error", err);
+        return;
+      }
+      for (const file of files) {
+        fs.unlink(path.join(tempDir, file), (err) => {});
+      }
+    });
+  }
+});
+
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 // error handler
 app.use((err, req, res, next) => {
